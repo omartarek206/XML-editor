6d03ff953cb1c6be10235c8cdb8f3a7da176b74b
karim-hafez77 <76133282+karim-hafez77@users.noreply.github.com>
2021-07-12 17:22:25 +0200

    imp stack and pushing tags in stack

--- Error_checking.py
+++ Error_checking.py
@@ -1,1 +1,31 @@
-print("test")
+from stack import *
+Tags=Stack()
+
+
+han=open("karim.txt",'r')
+
+#looping on every char in every line in xml file
+
+for line in han:
+    for i in range (len(line)):
+        #searching for <
+       if line[i]=='<':
+           #searching for next char after < if not ! nor ? nor / 
+           if line[i+1] !='?' and line[i+1] !='!' and line[i+1] !='/':
+               #searching for ' ' and > 
+               x=line.find(' ',i+1)
+               y=line.find('>')
+               if x==-1 :
+                   
+                    tag=line[1:y]    
+                    Tags.push(tag)             
+               else:
+                    tag=line[i+1:x]
+                    Tags.push(tag)
+                    
+
+
+print(Tags.pop())
+
+print(Tags.pop())
+
