9a0703c7268010c6733161eb8dc92a04a6e39ecf
karim-hafez77 <76133282+karim-hafez77@users.noreply.github.com>
2021-07-13 04:19:57 +0200

    chching consistency

--- Error_checking.py
+++ Error_checking.py
@@ -3,29 +3,54 @@ Tags=Stack()
 
 
 han=open("karim.txt",'r')
+flag=0
 
 #looping on every char in every line in xml file
-
+counter=0
 for line in han:
+    counter=counter+1
     for i in range (len(line)):
-        #searching for <
+       
+        #searching for <      
        if line[i]=='<':
+           
            #searching for next char after < if not ! nor ? nor / 
-           if line[i+1] !='?' and line[i+1] !='!' and line[i+1] !='/':
-               #searching for ' ' and > 
-               x=line.find(' ',i+1)
-               y=line.find('>')
-               if x==-1 :
+           if line[i+1] !='?' and line[i+1] !='!'  :
+               
+               if line[i+1] !='/':
+                    #searching for ' ' and > 
+                    x=line.find(' ',i+1)
+                    y=line.find('>',i+1)
+                    if x==-1 :                   
+                            tag=line[i+1:y]    
+                            Tags.push(tag)   
+                                     
+                    else:
+                            index=min(x,y)
+                            tag=line[i+1:index]
+                            Tags.push(tag)  
+                                 
+           
                    
-                    tag=line[1:y]    
-                    Tags.push(tag)             
+                                  
                else:
-                    tag=line[i+1:x]
-                    Tags.push(tag)
                     
+                    k=line.find('>',i+1)
+                    closing=line[i+2:k]
+                    if closing==Tags.peek():
+                        Tags.pop()
+                    else:
+                        print("error in line: ",counter)
+                        print("error detected in the following line: ")
+                        print(line)  
+                        
+   
+                
 
 
-print(Tags.pop())
-
-print(Tags.pop())
-
+if Tags.isEmpty():
+    print("xml is consistant")
+else:  
+    while Tags.isEmpty()==0:
+        print(Tags.pop())
+    print("xml isnot consistant")  
